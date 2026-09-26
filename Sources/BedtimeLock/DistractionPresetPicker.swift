5e8b36dac62ad2bba30c393bf75dcfa287a88b53
Ren Jopson <rin674167@gmail.com>
2026-09-26 18:52:33 +1000

    Add allow-until-midnight preset action

--- Sources/BedtimeLock/DistractionPresetPicker.swift
+++ Sources/BedtimeLock/DistractionPresetPicker.swift
@@ -88,6 +88,26 @@ struct DistractionPresetPicker: View {
                                 !state.status
                                     .distractionSettingsEditable
                             )
+                            .contextMenu {
+                                if enabled {
+                                    Button(
+                                        state.isDistractionPresetTemporarilyAllowed(
+                                            preset
+                                        )
+                                        ? "Allowed until midnight"
+                                        : "Allow until midnight"
+                                    ) {
+                                        state.allowDistractionPresetUntilEndOfToday(
+                                            preset
+                                        )
+                                    }
+                                    .disabled(
+                                        state.isDistractionPresetTemporarilyAllowed(
+                                            preset
+                                        )
+                                    )
+                                }
+                            }
                         }
                     }
                 }
